fff0469b444e2c664e90532b8732676bd5741222
Chester <chester_john24@yahoo.com>
2024-02-02 15:20:52 -0700

    Reports and code clean up

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -218,15 +218,6 @@
                 Clients
               </p>
             </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="pages/layout/top-nav.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Top Navigation</p>
-                </a>
-              </li>
-              
-            </ul>
           </li>
           <li class="nav-item">
             <a href="#" class="nav-link">
