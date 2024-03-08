b0a1af5f9f41f8d800528ab5f7f0170130e6ea18
HexaBridge <kobe4smallman@gmail.com>
2024-03-08 09:05:09 -0700

    Added addtional order functionality, the product still does not work, but I will fix soon, the order details and create pages as well have been added.

--- resources/views/Order/createOrder.blade.php
+++ resources/views/Order/createOrder.blade.php
@@ -67,9 +67,9 @@
 
                     <div class="card-footer">
                         <button type="button" id="addProductButton" class="btn btn-info">Add Product</button>
-                        <button type="submit" class="btn btn-success float 
--right">Create Order</button>
-</div>
+                        <button type="submit" class="btn btn-success float-right">Create Order</button>
+                        <a href="{{ route('orders.index') }}" class="btn btn-secondary float-right mr-2">Back to Orders</a>
+                    </div>
 </form>
 </div>
 </div><!-- /.container-fluid -->
