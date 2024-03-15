00545a7cabb974a0ebb8679e73709c1686f42d77
razelventura <ivy_razel.ventura@lethbridgecollege.ca>
2024-03-15 09:19:08 -0600

    Stubbed employee dashboard tabs

--- resources/views/dashboard/employeeCalendar.blade.php
+++ resources/views/dashboard/employeeCalendar.blade.php
@@ -1,11 +1,14 @@
 {{-- resources/views/dashboard/employeeCalendar.blade.php --}}
 <div class="wrapper">
     <!-- Calendar content here -->
-    <div id="employeeCalendar"></div>
+    <h2> Employee Calendar will be displayed here </h2>
+   <!-- <div id="employeeCalendar"></div>-->
 </div>
 
+<!--
 @push('scripts')
 <script>
 // FullCalendar initialization for Employee Calendar
 </script>
 @endpush
+-->
