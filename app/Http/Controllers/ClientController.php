087ca8924f534d66c5a24ab15e3b446430f24b1d
TonyCIT <113157040+TonyCIT@users.noreply.github.com>
2024-03-16 23:02:33 -0600

    Update ClientController.php Fix displaying the last 5 orders

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -150,7 +150,14 @@ public function store(Request $request)
     }
 }
 
+    public function lastOrders($id)
+    {
+        $client = Client::with(['orders' => function ($query) {
+            $query->latest('Request_DATE')->take(5);
+        }])->findOrFail($id);
 
+        return response()->json($client->orders);
+    }
 
     public function adminDashboard()
 {
